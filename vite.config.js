1920e05d85ed5b72b5b890f3cb71efe65e840f83
Mohitjain49 <mohitkjain49@gmail.com>
2025-02-20 01:41:01 -0500

    Changed directory style to be more AWS compatible.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   base: "/",
   plugins: [vue()],
+  ssgOptions: {
+    dirStyle: 'nested'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
