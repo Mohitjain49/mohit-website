015b40b6b7f31762708c6402efcf861f36f45027
Mohitjain49 <mohitkjain49@gmail.com>
2025-03-05 01:49:31 -0500

    Finished an initial draft of the KSU page.
    It's extremely basic, but it works alright for now.
    Also updated my resume for this website.

--- src/pages/education/KennesawStatePage.vue
+++ src/pages/education/KennesawStatePage.vue
@@ -10,6 +10,48 @@
             </a>
             <div class="ksu-page-header-caption"> {{ PAGE_DESC }} </div>
         </div>
+
+        <div id="CSE_1322" class="ksu-page-section" style="color: #EC2025"
+            v-observe-visibility="(isVisible) => { manageKennesawVisibilityTransition(isVisible, 'CSE_1322') }">
+
+            <a :href="KSU_CSE_1322_LINK" target="_blank" class="ksu-page-class-header">
+                <font-awesome-icon icon="fa-brands fa-java" />
+                <span> CSE 1322</span>
+            </a>
+            <div class="ksu-page-header-caption">
+                This is a basic programming class that taught me some fundamentals about coding. 
+                This class also helped me dig deeper into Java, a very important programming langauge for today's ecosystem.
+            </div>
+        </div>
+
+        <div id="CS_3410" class="ksu-page-section" style="color: #3E6E93"
+            v-observe-visibility="(isVisible) => { manageKennesawVisibilityTransition(isVisible, 'CS_3410') }">
+
+            <a :href="'https://www.mysql.com/'" target="_blank" class="ksu-page-class-header">
+                <font-awesome-icon icon="fa-database" />
+                <span> CS 3410</span>
+            </a>
+            <div class="ksu-page-header-caption">
+                This class taught me the fundamentals of databases and Structured Query Language (SQL). 
+                I am now applying the skills I learned in this class to make a new user database for 
+                <span><a :href="WORLDS_IVUE_LINK" target="_blank">Worlds iVue</a></span>.
+            </div>
+        </div>
+
+        <div id="SWE_3313" class="ksu-page-section last" style="color: #3E6E93"
+            v-observe-visibility="(isVisible) => { manageKennesawVisibilityTransition(isVisible, 'SWE_3313') }">
+
+            <a :href="KSU_SWE_3313_LINK" target="_blank" class="ksu-page-class-header">
+                <font-awesome-icon icon="fa-file-code" />
+                <span> SWE 3313</span>
+            </a>
+            <div class="ksu-page-header-caption">
+                This class taught me the fundamentals of software engineering, or thinking about stakeholders and 
+                code organization wehndeveloping an application. I am now applying the skills I learned in this class 
+                to improve the formatting of all of iVue's Websites, 
+                <span><a :href="WORLDS_IVUE_LINK" target="_blank">Worlds iVue</a></span>, and my personal projects.
+            </div>
+        </div>
         <WebFooter />
     </div>
 
@@ -34,13 +76,15 @@ import WebFooter from "@/components/WebFooter.vue";
 
 import { KSU_BACKGROUND } from '@/stores/ParticlesConfig.js';
 import { useWebsiteDataStore } from '@/stores/WebsiteData.js';
-import { KSU_LINK } from "@/stores/Objects.js";
+import { KSU_LINK, WORLDS_IVUE_LINK } from "@/stores/Objects.js";
 
 import { onMounted } from 'vue';
 import { useHead } from '@unhead/vue';
 
 const webData = useWebsiteDataStore();
 const KSU_STYLES_LINK = "https://styleguide.kennesaw.edu/";
+const KSU_CSE_1322_LINK = "https://www.kennesaw.edu/ccse/first-year-experience/cse-1322-sb.php";
+const KSU_SWE_3313_LINK = "https://advising.kennesaw.edu/explore_the_majors/software_engineering.php";
 
 const WEBSITE_PATH = "https://mohit-jain.com/ksu-edu";
 const PAGE_TITLE = "Mohit Jain | Kennesaw State University";
@@ -61,7 +105,7 @@ function closeNavBarDropdown() {
  * @param {String} id The id of the page section.
  */
 function manageKennesawVisibilityTransition(isVisible, id = "start") {
-    console.log
+    if(window.innerWidth < 450) { return; }
     const children = document.getElementById(id).children;
     const transition = (leftTransition = true) => {
         return (leftTransition ? "animate__lightSpeedInLeft" : "animate__lightSpeedInRight");
@@ -102,6 +146,7 @@ useHead({
 
 .ksu-page-section {
     padding-top: 50px;
+    padding-bottom: 20px;
     height: fit-content;
     min-height: var(--body-height);
     width: 100%;
@@ -109,17 +154,21 @@ useHead({
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    color: black;
 }
 .ksu-page-header-caption {
     height: fit-content;
     width: calc(100% - 50px);
     max-width: 1150px;
     border-radius: 10px;
-    color: black;
+    color: inherit;
     font-family: 'Montserrat', sans-serif;
     font-size: 33px;
     text-align: center;
     padding-bottom: 100px;
+}
+.ksu-page-header-caption a {
+    text-decoration: underline;
 }
 
 .ksu-page-header-img {
@@ -137,6 +186,21 @@ useHead({
     width: 100%;
     height: auto;
     user-select: none;
+}
+
+.ksu-page-class-header {
+    font-size: 120px;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: bold;
+    color: inherit;
+    padding: 10px 25px;
+    margin: 10px 0px;
+    border-radius: 10px;
+    transition: var(--default-transition);
+    width: fit-content;
+}
+.ksu-page-class-header:hover {
+    background-color: rgba(0, 0, 0, 0.25);
 }
 
 .ksu-credits-link {
@@ -179,11 +243,26 @@ useHead({
         width: calc(100% - 75px);
         padding: 5px 15px;
     }
+    .ksu-page-class-header {
+        font-size: 90px;
+    }
 }
 
 @media (max-width: 600px) {
+    .ksu-page-section {
+        min-height: 350px;
+        padding-bottom: 0px;
+    }
+    .ksu-page-section.last {
+        padding-bottom: 50px;
+    }
+
     .ksu-page-header-caption {
         font-size: 17px;
+        padding-bottom: 20px;
+    }
+    .ksu-page-class-header {
+        font-size: 50px;
     }
 }
 </style>
