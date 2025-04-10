d5811b94a0b4ed9894cdede1e2fad529e46d9b1a
Mohitjain49 <mohitkjain49@gmail.com>
2025-04-10 11:17:27 -0400

    Fixed Build Issue (Probably)

--- src/pages/ResumeNative.vue
+++ src/pages/ResumeNative.vue
@@ -3,17 +3,18 @@
 <ResumeWidgets />
 
 <div id="resume-container">
-    <iframe :src="resume" height="100%" width="100%"></iframe>
+    <iframe :src="resume_pdf" height="100%" width="100%"></iframe>
 </div>
 </template>
 
 <script setup>
 import NavigationMain from "../components/NavigationMain.vue";
 import ResumeWidgets from "../components/ResumeWidgets.vue";
-import resume from "/Mohit_Jain_Resume.pdf";
 
 import { getMeta } from "../stores/GetMeta.js";
 import { useHead } from "@unhead/vue";
+
+const resume_pdf = "https://www.mohit-jain.com/Mohit_Jain_Resume.pdf";
 
 useHead(getMeta("Mohit Jain | My Resume", "resume/pdf",
     "Feel free to take a look at my resume."
