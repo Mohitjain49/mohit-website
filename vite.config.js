5b33efb11e150c3188bc393b136fb6c96741a6e7
Mohitjain49 <mohitkjain49@gmail.com>
2025-06-11 12:58:00 -0400

    Updated Website Version + PWA Workbox.
    The PWA should now precache the fonts so that it works offline.

--- vite.config.js
+++ vite.config.js
@@ -31,9 +31,11 @@ export default defineConfig({
         VitePWA({
             registerType: "autoUpdate",
             devOptions: { enabled: false },
+            includeAssets: ['**/*.woff2', '**/*.woff'],
+
             workbox: {
-                cacheId: "v2.6.7",
-                globPatterns: ['**/*.{js,css,html,png,svg,pdf,webp,jpg,jpeg}'],
+                cacheId: "v2.6.8",
+                globPatterns: ['**/*.{js,css,html,png,svg,pdf,webp,jpg,jpeg,woff2,woff,ttf,eot}'],
                 maximumFileSizeToCacheInBytes: 3000000
             },
 
