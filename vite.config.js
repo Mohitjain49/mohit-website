877da0ff64526d0d3558902ee7ab8403f5f8f6f8
Mohitjain49 <mohitkjain49@gmail.com>
2025-08-24 17:06:55 -0400

    Caching worker files now

--- vite.config.js
+++ vite.config.js
@@ -57,7 +57,7 @@ export default defineConfig({
 
             workbox: {
                 cacheId: "v2.9.0",
-                globPatterns: ['**/*.{js,css,html,png,svg,pdf,webp,jpg,jpeg,woff2,woff,ttf,eot,md,wav,xml,txt,xsl}'],
+                globPatterns: ['**/*.{js,css,html,mjs,png,svg,pdf,webp,jpg,jpeg,woff2,woff,ttf,eot,md,wav,xml,txt,xsl}'],
                 maximumFileSizeToCacheInBytes: 3000000,
                 navigateFallback: "/index.html",
                 navigateFallbackDenylist: [/\.xml$/, /\.txt$/, /\.xsl$/]
