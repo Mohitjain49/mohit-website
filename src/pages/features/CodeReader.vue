ea8790c47cc6fb714e8a88d2f331f60dbfcc487c
Mohitjain49 <mohitkjain49@gmail.com>
2025-09-01 22:28:36 -0400

    Continued making more updates to The Code Scanner Page.
    All logic is now stored within a Pinia Store for more convenience.
    Scanned items are now saved in the local storage for the website and updated as items are added and removed.
    Links that lead to external websites now can be clicked on.

--- src/pages/features/CodeReader.vue
+++ src/pages/features/CodeReader.vue
@@ -1,3 +1,14 @@
+<script setup>
+import { QrcodeStream } from 'vue-qrcode-reader';
+const codeScanner = useCodeScannerStore();
+
+onMounted(() => { codeScanner.mountCodeScanner(); });
+onBeforeUnmount(() => { codeScanner.deactivateCamera(); });
+useHead(getMeta("Mohit Jain | Barcode & Qrcode Reader", "code-reader",
+    "This page is capable of reading the values of Barcodes and QR Codes."
+));
+</script>
+
 <template>
 <client-only>
     <vue-particles id="particlests" :options="FEATURES_BACKGROUND"></vue-particles>
@@ -6,24 +17,28 @@
 <main id="code-reader-page" class="personal-web-body">
     <div class="code-scanner-main">
         <div class="scanner-component-container">
-            <QrcodeStream @detect="onDetect" @camera-on="onCameraActive"
-                :formats="BARCODE_FORMATS"
-                :style="{ 'display': (!cameraActive ? 'none' : '') }"
+            <QrcodeStream @detect="(event) => {codeScanner.onDetectCode(event)}"
+                @camera-on="(event) => {codeScanner.onCameraActive(event)}"
+                @camera-off="(event) => {codeScanner.deactivateCamera(event)}"
+                :formats="codeScanner.BARCODE_FORMATS"
+                :style="{ 'display': (!codeScanner.cameraActive ? 'none' : '') }"
             />
-            <div v-if="!cameraActive" class="scanner-component-inactive"> Waiting On Camera... </div>
+            <div v-if="!codeScanner.cameraActive" class="scanner-component-inactive"> Waiting On Camera... </div>
         </div>
         <div class="code-scanner-console">
             <div class="scanner-console-top">
                 <div class="scanner-console-header"> <FontAwesomeIcon icon="fa-terminal" /> Console </div>
                 <div class="scanner-console-utils">
-                    <button @click="clearAllScannedItems()" title="Delete All Scanned Items" style="color: red;">
+                    <button @click="codeScanner.clearAllScannedItems()" title="Delete All Scanned Items" style="color: red;">
                         <FontAwesomeIcon icon="fa-trash" />
                     </button>
                 </div>
             </div>
             <div class="scanner-console-body">
-                <div class="scanned-code-item" v-for="item in scannedItemsArray">
-                    <p> {{ truncate(item.value, 25) }} </p>
+                <div class="scanned-code-item" v-for="item in codeScanner.scannedItems">
+                    <p v-if="!item.onlineLink"> {{ truncate(item.value, 25) }} </p>
+                    <a v-else :href="item.value" :title="('Open: ' + item.value)"> {{ truncate(item.value, 25) }} </a>
+
                     <button title="Open Info For Scanned Item" @click="() => {}">
                         <FontAwesomeIcon icon="fa-bars-staggered" />
                     </button>
@@ -34,73 +49,6 @@
 </main>
 <FeaturesReturnWidget />
 </template>
-
-<script setup>
-import { QrcodeStream } from 'vue-qrcode-reader';
-const cameraActive = ref(false);
-
-/**
- * @type {import('vue').Ref<Array<{ value: String, format: String }>>}
- * This is the array of codes scanned by the camera.
- */
-const scannedItemsArray = ref([{ value: "https://www.mohit-jain.com/", format: "qr_code" }]);
-/**
- * This function is run whenever a visior activates their camera for the webpage.
- */
-function onCameraActive(event) {
-    if(import.meta.env.DEV) { console.log(event); }
-    cameraActive.value = true;
-}
-
-/**
- * This is triggered every time a new code is scanned.
- * @param {Array<{ rawValue: String, format: String }>} event The object made by the event.
- */
-function onDetect(event) {
-    if(import.meta.env.DEV) { console.log(event); }
-    triggerScanSound();
-    for(let i = 0; i < event.length; i++) {
-        const item = event[i];
-        scannedItemsArray.value.push({ value: item.rawValue, format: item.format });
-    }
-}
-
-/**
- * This function clears all scanned items from the list.
- */
-function clearAllScannedItems() {
-    scannedItemsArray.value = [];
-}
-
-const BARCODE_FORMATS = [
-    "aztec",
-    "code_128",
-    "code_39",
-    "code_93",
-    "codabar",
-    "databar",
-    "databar_expanded",
-    "data_matrix",
-    "dx_film_edge",
-    "ean_13",
-    "ean_8",
-    "itf",
-    "maxi_code",
-    "micro_qr_code",
-    "pdf417",
-    "qr_code",
-    "rm_qr_code",
-    "upc_a",
-    "upc_e",
-    "linear_codes",
-    "matrix_codes"
-];
-
-onMounted(() => { initWebData(); });
-useHead(getMeta("Mohit Jain | Barcode & Qrcode Reader", "code-reader",
-    "This page is capable of reading the values of Barcodes and QR Codes."
-));
-</script>
 
 <style scoped>
 .personal-web-body#code-reader-page {
@@ -221,11 +169,14 @@ useHead(getMeta("Mohit Jain | Barcode & Qrcode Reader", "code-reader",
     align-items: center;
     flex-direction: row;
 }
-.scanned-code-item p {
+.scanned-code-item p, .scanned-code-item a {
     font-family: 'Montserrat', sans-serif;
     color: inherit;
     font-size: 15px;
     font-weight: bold;
+}
+.scanned-code-item a:hover {
+    text-decoration: underline;
 }
 
 .scanned-code-item button {
@@ -237,6 +188,10 @@ useHead(getMeta("Mohit Jain | Barcode & Qrcode Reader", "code-reader",
     border: 1px solid;
     border-radius: 5px;
     padding: 4px;
+    transition: var(--default-transition);
+}
+.scanned-code-item button:hover {
+    background-color: black;
 }
 
 @media (max-width: 1050px) {
---
+++ src/stores/CodeScannerStore.js
@@ -0,0 +1,100 @@
+export const useCodeScannerStore = defineStore("code-scanner-store", () => {
+    const SCANNED_ITEMS_KEY = "mohit-codeScanner-items";
+
+    /**
+     * @type {import('vue').Ref<Array<{ value: String, format: String, onlineLink: Boolean }>>}
+     * This is the array of codes scanned by the camera.
+     */
+    const scannedItems = ref([{ value: "https://www.mohit-jain.com/", format: "qr_code", onlineLink: true }]);
+    const cameraActive = ref(false);
+
+    /**
+     * This function mounts the page hosting the code scanner.
+     */
+    function mountCodeScanner() {
+        initWebData();
+        const lastScannedItems = localStorage.getItem(SCANNED_ITEMS_KEY);
+        scannedItems.value = ((lastScannedItems == null) ? [] : JSON.parse(lastScannedItems));
+    }
+
+    /**
+     * This function is run whenever a visior activates their camera for the webpage.
+     */
+    function onCameraActive(event) {
+        if(import.meta.env.DEV) { console.log(event); }
+        cameraActive.value = true;
+    }
+
+    /**
+     * This is triggered every time a new code is scanned.
+     * @param {Array<{ rawValue: String, format: String }>} event The object made by the event.
+     */
+    function onDetectCode(event) {
+        if(import.meta.env.DEV) { console.log(event); }
+        for(let i = 0; i < event.length; i++) {
+            const item = event[i];
+            const onlineLink = (item.rawValue.startsWith("https://") ||
+                item.rawValue.startsWith("http://") ||
+                item.rawValue.startsWith("mailto:") ||
+                item.rawValue.startsWith("tel:")
+            );
+            scannedItems.value.push({ value: item.rawValue, format: item.format, onlineLink });
+        }
+
+        triggerScanSound();
+        setLocalStorageObj();
+    }
+
+    /**
+     * This function clears all scanned items from the list.
+     */
+    function clearAllScannedItems() {
+        scannedItems.value = [];
+        setLocalStorageObj()
+    }
+
+    /**
+     * This function sets the "cameraActive" boolean to false.
+     * Note that this doesn't actually deactivate the camera.
+     */
+    function deactivateCamera(event) {
+        if(import.meta.env.DEV) { console.log(event); }
+        cameraActive.value = false;
+    }
+
+    /**
+     * This function sets the local storage object so that visitors can access scanned items later.
+     */
+    function setLocalStorageObj() {
+        localStorage.setItem(SCANNED_ITEMS_KEY, JSON.stringify(scannedItems.value));
+    }
+
+    const BARCODE_FORMATS = [
+        "aztec",
+        "code_128",
+        "code_39",
+        "code_93",
+        "codabar",
+        "databar",
+        "databar_expanded",
+        "data_matrix",
+        "dx_film_edge",
+        "ean_13",
+        "ean_8",
+        "itf",
+        "maxi_code",
+        "micro_qr_code",
+        "pdf417",
+        "qr_code",
+        "rm_qr_code",
+        "upc_a",
+        "upc_e",
+        "linear_codes",
+        "matrix_codes"
+    ];
+
+    return { scannedItems, cameraActive, BARCODE_FORMATS,
+        mountCodeScanner, clearAllScannedItems,
+        onCameraActive, onDetectCode, deactivateCamera
+    }
+});
