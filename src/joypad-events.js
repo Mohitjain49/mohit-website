9e70eeec18292763da085606b80f2782a2f7118e
Mohit Jain <125706619+Mohitjain49@users.noreply.github.com>
2025-09-08 21:17:01 -0400

    Version Update 3.0.2 (#45)
    * Replaced Joypad.js with native code.
    
    Now all events from a gamepad are made without Joypad.js and work exactly the same. The events now have moderate type support as well.
    Uninstalled Joypad.js
    
    * Added a new "button hold" event.
    
    This tracks when a button is being held down and how long it is held down.
    I have replaced some functionality when adjusting the max cursor speed and the volume so that it uses the new event rather than intervals.
    
    * Updated a resume link.
    
    On the top, I edited my website link to use www.mohit-jain.com instead of mohit-jain.com.
    When a user clicks on it on the main resume page, it will guide the user back to the home page rather than to a separate tab.

--- src/joypad-events.js
+++ src/joypad-events.js
@@ -1,23 +1,93 @@
-import 'joypad.js';
+let prevButtons = [];
+let holdTimes = [];
+const AXIS_MOVEMENT_THRESHOLD = 0.05;
 
-joypad.set({
-    axisMovementThreshold: 0.05,
-})
+/**
+ * This function polls the gamepad each frame to check for if any change occurred on the controller.
+ * If something did move, then it releases an event.
+ */
+function pollGamepad() {
+    const gamepads = navigator.getGamepads();
+    if(!gamepads) { return };
 
-joypad.on("connect", (e) => {
+    for (const gp of gamepads) {
+        if(!gp) { continue };
+
+        gp.buttons.forEach((btn, index) => {
+            const wasPressed = (prevButtons[gp.index]?.[index] || false);
+            const isPressed = btn.pressed;
+
+            // This releases an event for if a button was just pressed.
+            if(isPressed && !wasPressed) {
+                const event = new CustomEvent("gamepadbuttondown", {
+                    detail: new GamepadButtonStatusEvent(gp, index, "down", 0)
+                });
+                window.dispatchEvent(event);
+            }
+
+            // This releases an event for if a button was just released.
+            if(!isPressed && wasPressed) {
+                const event = new CustomEvent("gamepadbuttonup", {
+                    detail: new GamepadButtonStatusEvent(gp, index, "up", -1)
+                });
+                window.dispatchEvent(event);
+            }
+
+            // This releases an event for if a button is being held down.
+            if(isPressed && wasPressed) {
+                const holdTime = holdTimes[gp.index][index];
+                const event = new CustomEvent("gamepadbuttonhold", {
+                    detail: new GamepadButtonStatusEvent(gp, index, "hold", holdTime)
+                });
+                window.dispatchEvent(event);
+            }
+        });
+
+        // This releases an event every frame a joystick is moving passed the threshold.
+        gp.axes.forEach((axis, index) => {
+            if(axis <= AXIS_MOVEMENT_THRESHOLD && axis >= -AXIS_MOVEMENT_THRESHOLD) { return; }
+            const event = new CustomEvent("gamepadaxismove", {
+                detail: new GamepadAxisMoveEvent(gp, index, axis)
+            });
+            window.dispatchEvent(event);
+        });
+
+        // This sets the current frame button status as the previous buttons for the next frame.
+        const newButtons = gp.buttons.map(b => b.pressed);
+        prevButtons[gp.index] = newButtons;
+
+        if(!holdTimes[gp.index]) {
+            holdTimes[gp.index] = newButtons.map(pressed => { return (pressed ? 0 : 1) })
+        } else {
+            for(let i = 0; i < newButtons.length; i++) {
+                holdTimes[gp.index][i] = (newButtons[i] ? (holdTimes[gp.index][i] + 1) : -1);
+            }
+        }
+    }
+
+    // Loops the function to continue reading the gamepad each frame.
+    requestAnimationFrame(pollGamepad);
+}
+
+/**
+ * This event runs whenever a new gamepad is connected.
+ */
+window.addEventListener("gamepadconnected", (e) => {
     useGamepadStore().startGamepadConnectedInterval();
     if(import.meta.env.DEV) { console.log(e); }
-})
+});
 
-joypad.on("button_press", (e) => {
-    const buttonIndex = parseInt(e.detail.buttonName.split('_')[1], 10);
+/**
+ * This event runs whenever a button is pressed down.
+ */
+window.addEventListener("gamepadbuttondown", (e) => {
+    /** @type {GamepadButtonStatusEvent} */
+    const event = e.detail;
+    const buttonIndex = event.button;
     // if(import.meta.env.DEV) { console.log(buttonIndex); }
 
-    if(buttonIndex == 12 || buttonIndex == 13) {
-        useGamepadStore().setCursorYInterval(buttonIndex == 12);
-    }
-    if(buttonIndex == 14 || buttonIndex == 15) {
-        useGamepadStore().setCursorXInterval(buttonIndex == 14);
+    if(buttonIndex >= 12 && buttonIndex <= 15) {
+        useGamepadStore().manageCustomCursorWithDpad(buttonIndex - 12);
     }
 
     if(buttonIndex >= 0 && buttonIndex <= 3) {
@@ -29,48 +99,81 @@ joypad.on("button_press", (e) => {
     }
 
     if(buttonIndex == 5) {
-        useGamepadStore().startCursorSpeedInterval(true);
+        useGamepadStore().addToMaxCursorSpeed(1);
     }
     if(buttonIndex == 4) {
-        useGamepadStore().startCursorSpeedInterval(false);
+        useGamepadStore().addToMaxCursorSpeed(-1);
     }
 
     if(buttonIndex == 7) {
-        useAudioStore().setVolumeInterval(1);
+        useAudioStore().addToVolume(1);
     }
     if(buttonIndex == 6) {
-        useAudioStore().setVolumeInterval(-1);
+        useAudioStore().addToVolume(-1);
     }
 });
 
-joypad.on("button_release", (e) => {
-    const buttonIndex = parseInt(e.detail.buttonName.split('_')[1], 10);
+/**
+ * This event runs whenever a button is released.
+ */
+window.addEventListener("gamepadbuttonup", (e) => {
+    /** @type {GamepadButtonStatusEvent} */
+    const event = e.detail;
+    const buttonIndex = event.button;
     // if(import.meta.env.DEV) { console.log(buttonIndex); }
 
-    if(buttonIndex == 12 || buttonIndex == 13) {
-        useGamepadStore().stopCursorInterval("y")
-    }
-    if(buttonIndex == 14 || buttonIndex == 15) {
-        useGamepadStore().stopCursorInterval("x")
-    }
-
-    if(buttonIndex == 4 || buttonIndex == 5) {
-        useGamepadStore().stopCursorSpeedInterval();
-    }
     if(buttonIndex == 6 || buttonIndex == 7) {
-        useAudioStore().stopVolumeInterval();
+        useAudioStore().showVolumeGamepadMenu = false;
+    }
+    if(buttonIndex == 4 || buttonIndex == 5) {
+        useGamepadStore().showCursorSpeedMenu = false;
     }
 });
 
-joypad.on("axis_move", (e) => {
+/**
+ * This event runs whenever a button is held down during a frame.
+ */
+window.addEventListener("gamepadbuttonhold", (e) => {
+    /** @type {GamepadButtonStatusEvent} */
+    const event = e.detail;
+    const buttonIndex = event.button;
+    const holdFrames = event.holdFrames;
+    // if(import.meta.env.DEV) { console.log(event); }
+
+    if(buttonIndex >= 12 && buttonIndex <= 15) {
+        useGamepadStore().manageCustomCursorWithDpad(buttonIndex - 12);
+    }
+
+    if(holdFrames < 50) { return; }
+    if(buttonIndex == 5 && holdFrames % 5 == 0) {
+        useGamepadStore().addToMaxCursorSpeed(1);
+    }
+    if(buttonIndex == 4 && holdFrames % 5 == 0) {
+        useGamepadStore().addToMaxCursorSpeed(-1);
+    }
+
+    if(buttonIndex == 7 && holdFrames % 3 == 0) {
+        useAudioStore().addToVolume(1);
+    }
+    if(buttonIndex == 6 && holdFrames % 3 == 0) {
+        useAudioStore().addToVolume(-1);
+    }
+});
+
+/**
+ * This event runs whenever a joystick is moved far enough from its initial position.
+ */
+window.addEventListener("gamepadaxismove", (e) => {
+    /** @type {GamepadAxisMoveEvent} */
     const event = e.detail;
     // if(import.meta.env.DEV) { console.log(event); }
 
-    if(event.stickMoved === "left_stick") {
+    if(event.stick === "left_stick") {
         useGamepadStore().manageCustomCursor(event);
-    } else if(event.axis == 3) {
-        useGamepadStore().initScrollYBy('bottom',
-            (30 * event.axisMovementValue)
-        );
+    } else if(event.axisIndex == 3) {
+        useGamepadStore().initScrollYBy('bottom', (30 * event.movement));
     }
-})
+});
+
+// Starts the process of sending events.
+requestAnimationFrame(pollGamepad);
