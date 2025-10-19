a24f079b8d591edb79f5fac89913065ebec9399c
Mohitjain49 <mohitkjain49@gmail.com>
2025-10-19 16:54:24 -0400

    Updated the deploy script.
    It can now take in the folder name for what build output the user would like to use, defaulting to 'dist'.
    It also deletes ALL current files in the bucket before pushing new ones in.
    Arguments can now be passed in to disable the deletion and the cache invalidation.

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -1,35 +1,46 @@
+// NPM Install Command: "npm install -D @aws-sdk/client-s3 @aws-sdk/client-cloudfront dotenv mime-types"
+// NOTE: This command does NOT work correctly when bucket versioning is enabled.
+
 import 'dotenv/config'; // Used To import .env variables independent from Vite.
 import * as fs from "fs"; // The File System Module. Used to find and get data from files in the build output.
 import * as path from 'path'; // The Path Module. Used to create path names and file names.
 import { lookup } from 'mime-types'; // The lookup function here can generate the MIME type for every file we fetch.
 
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"; // This module allows us to send files to an Amazon S3 bucket.
+import { S3Client, PutObjectCommand, DeleteObjectsCommand, paginateListObjectsV2 } from "@aws-sdk/client-s3"; // This module allows us to remove and send files to an Amazon S3 bucket.
 import { CloudFrontClient, CreateInvalidationCommand } from '@aws-sdk/client-cloudfront'; // This module allows us invalidate a CloudFront Cache.
 
-// NPM Install Command: "npm install -D @aws-sdk/client-s3 @aws-sdk/client-cloudfront dotenv mime-types"
-
+const BUILD_OUTPUT = (process.env.BUILD_OUTPUT ?? 'dist'); // This is the name of your folder where your application was built for deployment. Defaults to 'dist'.
 const AWS_REGION = process.env.AWS_REGION; // This is the region where both the S3 bucket and CloudFront Distribution should be located (e.g. "us-east-1").
 const AWS_BUCKET = process.env.AWS_BUCKET; // This is the name of the AWS bucket to send messages to. (e.g. "mohit-website").
 const AWS_CLOUDFRONT_DIST_ID = process.env.AWS_CLOUDFRONT_DIST_ID; // This is the ID of your CloudFront Distribution (e.g. "EDFDVBD632BHDS5").
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID; // This serves as the access key for your IAM account (e.g. AKIAIOSFODNN7EXAMPLE).
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY; // This serves as the access key for your IAM account (e.g. wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY).
-const outDir = path.resolve('dist'); // The path that leads to the build output.
 
+// console.log(BUILD_OUTPUT)
 // console.log(AWS_REGION);
 // console.log(AWS_BUCKET);
 // console.log(AWS_CLOUDFRONT_DIST_ID);
 // console.log(AWS_ACCESS_KEY_ID);
 // console.log(AWS_SECRET_ACCESS_KEY);
 
-// If any of the 5 environment variables are not defined, this will throw an error before the script starts.
-if(!AWS_REGION || !AWS_BUCKET || !AWS_CLOUDFRONT_DIST_ID || !AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) {
-    throw new Error("The environment variables are not properly configured.");
+const outDir = path.resolve(BUILD_OUTPUT); // The path that leads to the build output.
+const args = process.argv.slice(2); // These are the arguments for altering how the script works.
+
+const NO_DELETION = (args.findIndex(item => item === "--no-delete") == -1);
+const NO_INVALIDATION = (args.findIndex(item => item === "--no-invalidation") == -1);
+
+// If any of the 4 required environment variables are not defined, this will throw an error before the script starts.
+if(!AWS_REGION || !AWS_BUCKET || !AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) {
+    throw new Error("The mandatory environment variables are not properly configured.");
+}
+if(!AWS_CLOUDFRONT_DIST_ID && !NO_INVALIDATION) {
+    throw new Error("No CloudFront Distribution ID Provided.");
 }
 
 /**
- * --------------------------------------------------------------
- * This section specifically pushes the build files to Amazon S3.
- * --------------------------------------------------------------
+ * ----------------------------------------------------------
+ * This section specifically handles commands with Amazon S3.
+ * ----------------------------------------------------------
  */
 
 /** The Client for Amazon S3. */
@@ -42,15 +53,15 @@ const s3Client = new S3Client({
 });
 
 /**
- * This function sends an Put Object command via the S3 client.
- * @param {PutObjectCommand} command the command for the S3 client to send.
+ * This function sends a command via the S3 client.
+ * @param {PutObjectCommand | DeleteObjectsCommand} command the command for the S3 client to send.
  */
-async function sendPutObjectCommand(command) {
+async function sendS3Command(command) {
     return await s3Client.send(command);
 }
 
 /**
- * This function returns all the commands that will be sent by the S3 client.
+ * This function returns all the put object commands that will be sent by the S3 client.
  */
 function getPutObjectCommands() {
     const allFiles = fs.readdirSync(outDir, { recursive: true }); // All the filenames from the build output.
@@ -72,6 +83,33 @@ function getPutObjectCommands() {
     }
     return commands;
 }
+
+/**
+ * This function returns commands to delete ALL the objects currently in an array.
+ */
+async function getDeleteObjectCommands() {
+    // This gets all the current objects in the bucket.
+    const paginator = paginateListObjectsV2(
+        { client: s3Client },
+        { Bucket: AWS_BUCKET }
+    );
+
+    // This gets all the keys.
+    const objectKeys = [];
+    for await (const { Contents } of paginator) {
+        objectKeys.push(...Contents.map((obj) => ({ Key: obj.Key })));
+    }
+
+    // This creates and returns the commands.
+    const deleteCommands = [];
+    while(objectKeys.length != 0) {
+        deleteCommands.push(new DeleteObjectsCommand({
+            Bucket: AWS_BUCKET,
+            Delete: { Objects: objectKeys.splice(0, Math.min(objectKeys.length, 1000)) }
+        }));
+    }
+    return deleteCommands;
+};
 
 /**
  * ---------------------------------------------------------------------------------------------
@@ -110,17 +148,29 @@ async function sendCloudfrontInvalidation() {
 
 /** This is the main function to run at the end once all objects are initialized. */
 async function main() {
+    // Unless an argument is passed in, this deletes all current files in the bucket.
+    if(!NO_DELETION) {
+        const deleteCommands = getDeleteObjectCommands();
+        for(let j = 0; j < length; j++) { await sendS3Command(deleteCommands[j]); }
+        console.log(`✅ Deleted All Files From Bucket!\n`);
+    }
+
     const commands = getPutObjectCommands();
     const length = commands.length;
 
+    // This sends all the files in the build output to the bucket, replacing any files with the same key.
     for(let i = 0; i < length; i++) {
-        await sendPutObjectCommand(commands[i]);
+        await sendS3Command(commands[i]);
         console.log(`✅ Uploaded file ${i + 1} of ${length}: ${commands[i].input.Key}`);
     }
+    console.log(`✅ Uploaded All Files To Your Bucket!\n`);
 
-    await sendCloudfrontInvalidation();
-    console.log("✅ Completed CloudFront Cache Invalidation!");
-    console.log("All Files Uploaded");
+    // Unless an argument is passed in, this invalidates a cloudfront cache.
+    if(!NO_INVALIDATION) {
+        await sendCloudfrontInvalidation();
+        console.log("✅ Completed CloudFront Cache Invalidation!");
+    }
+    console.log("Script Complete!");
 }
 
 await main();
