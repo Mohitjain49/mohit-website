eba8c37aa9257957c18b92a92bce223d294d5c59
Mohitjain49 <mohitkjain49@gmail.com>
2025-10-30 13:22:58 -0400

    Updated dependencies and project version number.

--- vite.config.js
+++ vite.config.js
@@ -87,7 +87,7 @@ export default defineConfig({
             includeAssets: ['**/*.woff2', '**/*.woff'],
 
             workbox: {
-                cacheId: `v3.3.3-${Date.now()}`,
+                cacheId: `v3.3.4-${Date.now()}`,
                 globPatterns: ['**/*.{js,css,html,mjs,png,svg,pdf,webp,jpg,jpeg,woff2,woff,ttf,eot,md,wav,xml,txt,xsl,mp3}'],
                 maximumFileSizeToCacheInBytes: 3000000,
                 navigateFallback: "/index.html",
