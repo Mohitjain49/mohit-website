2840f33b61a2bb75cc2f1d7469dbff9e977c1ffc
Mohitjain49 <mohitkjain49@gmail.com>
2025-11-19 09:33:14 -0500

    Made a couple edits. See Description.
    Updated dependencies and project version number.
    Added more redirect routes and fixed bugs concerning the particles background for the invalid/redirect page.

--- src/pages/InvalidRoute.vue
+++ src/pages/InvalidRoute.vue
@@ -1,6 +1,6 @@
 <template>
-<ParticlesBackground :particlesOptions="(backgroundType == 0 ? INVALID_BACKGROUND : REDIRECT_BACKGROUND)" />
-<main id="invalid" class="personal-web-body">
+<ParticlesBackground v-if="(backgroundType != -1)" :particlesOptions="((backgroundType == 0) ? INVALID_BACKGROUND : REDIRECT_BACKGROUND)" />
+<main id="invalid" class="personal-web-body" :style="bodyBackground">
     <h1 class="incomplete-title"> {{ PAGE_DESC }} </h1>
     <!-- <div class="incomplete-subtitle"> {{ '' }} </div> -->
     
@@ -19,7 +19,11 @@ const router = useRouter();
 
 const PAGE_TITLE = ref("Mohit Jain | 404 Error");
 const PAGE_DESC = ref("404 - Page Not Found");
-const backgroundType = ref(0);
+
+const backgroundType = ref(-1);
+const bodyBackground = computed(() => {
+    return { background: ((backgroundType.value == -1) ? "var(--webpage-static-background)" : "transparent")}
+});
 
 const routePath = computed(() => { return router.currentRoute.value.path; });
 const metaTags = computed(() => { return getMeta(PAGE_TITLE.value, "404", PAGE_DESC.value); });
@@ -29,25 +33,27 @@ onMounted(() => {
     // This section redirects to a particular page in the website if the user typed in a particular route.
     const internalRoute = INTERNAL_REDIRECTS.findIndex(item => checkRedirectRoute(item.routes));
     if(internalRoute != -1) {
-        startRedirect();
+        startRedirect(true);
         router.replace(INTERNAL_REDIRECTS[internalRoute].replacement);
     }
 
     // This section redirects to another website if the user typed in a particular route.
     const externalRoute = EXTERNAL_REDIRECTS.findIndex(item => checkRedirectRoute(item.routes));
     if(externalRoute != -1) {
-        startRedirect();
+        startRedirect(false);
         window.location.replace(EXTERNAL_REDIRECTS[externalRoute].replacement);
     }
+    if(internalRoute == -1 && externalRoute == -1) { backgroundType.value = 0; }
 });
 
 /**
  * This function runs whenever the website starts redirecting the user to another page.
+ * @param {Boolean} internal If true, this indicates that the page will start to redirect to an internal route.
  */
-function startRedirect() {
+function startRedirect(internal = true) {
     PAGE_TITLE.value = "Mohit Jain | Redirecting...";
     PAGE_DESC.value = "Redirecting...";
-    backgroundType.value = 1;
+    backgroundType.value = (internal ? -1 : 1);
 }
 
 /**
@@ -72,6 +78,13 @@ const INTERNAL_REDIRECTS = [
     { routes: ["/exp"], replacement: "/experience/" },
     { routes: ["/icon", "/static-icons"], replacement: "/icons" },
     { routes: ["/documents"], replacement: "/#documents" },
+
+    { routes: ["/documents/resume"], replacement: "/resume" },
+    { routes: ["/documents/resume/qrcode"], replacement: "/resume/qrcode" },
+    { routes: ["/documents/resume/markdown"], replacement: "/resume/markdown" },
+
+    { routes: ["/documents/create-github-repo"], replacement: "/create-github-repo" },
+    { routes: ["/documents" + FCS_CERTIFICATE_ROUTE], replacement: FCS_CERTIFICATE_ROUTE },
 ];
 
 // This is a list of redirects to other websites.
@@ -82,7 +95,8 @@ const EXTERNAL_REDIRECTS = [
 
     { routes: ["/code", '/codesandbox', '/code-sandbox'], replacement: PERSONAL_WEBSITE_CODE_SANDBOX },
     { routes: ["/repo"], replacement: PERSONAL_WEBSITE_REPOSITORY_LINK },
-    { routes: ["/deploy-script"], replacement: PERSONAL_DEPLOY_SCRIPT_LINK },
+    { routes: ["/deploy-script", "/documents/deploy-script"], replacement: PERSONAL_DEPLOY_SCRIPT_LINK },
+    { routes: ["/documents/sitemap"], replacement: PERSONAL_SITEMAP_LINK },
 
     { routes: ['/wiv', '/worlds-ivue', "/ivue/world"], replacement: WORLDS_IVUE_LINK },
     { routes: ["/ivuemedia", "/ivue/media"], replacement: IVUE_MEDIA_WEBSITE_LINK },
