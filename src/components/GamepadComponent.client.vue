246578057bd14a3d03df639f3baaf9296b362bc3
Mohitjain49 <mohitkjain49@gmail.com>
2025-11-21 13:43:58 -0500

    Made a new element for changing the volume with a gamepad.
    It appears at the top rather than the center of the screen and it looks much better.
    I also put the unplugin-vue-components resolvers in their own file.

--- src/components/GamepadComponent.client.vue
+++ src/components/GamepadComponent.client.vue
@@ -13,6 +13,19 @@ const audioStore = useAudioStore();
     />
 </template>
 
+<Transition name="gamepad-volumeMeter-transition" fade>
+    <div class="new-volume-meter" v-if="audioStore.showVolumeGamepadMenu">
+        <button @click="audioStore.setAudioMuted('toggle')" :title="audioStore.volumeInputTitle">
+            <FontAwesomeIcon :icon="audioStore.volumeInputIcon" />
+        </button>
+        <input type="range" min="0" max="100" title="Volume Meter for the click sound."
+            v-model="audioStore.volumeInput" disabled
+            @input="audioStore.changeAudioVolume()"
+        />
+        <span> {{ (audioStore.volumeInput + '%') }} </span>
+    </div>
+</Transition>
+
 <Transition name="cursorTitle-transition" fade>
     <div v-if="gamepadStore.cursorElementTitle !== ''" class="custom-cursor-titlePopup">
         {{ truncate(gamepadStore.cursorElementTitle, 70) }}
@@ -27,13 +40,13 @@ const audioStore = useAudioStore();
     </div>
 </Transition>
 
-<Transition name="cursorSense-transition" fade>
+<!-- <Transition name="cursorSense-transition" fade>
     <div v-if="audioStore.showVolumeGamepadMenu" class="custom-cursor-sensitivity volume">
         <h1> Volume </h1>
         <input type="range" v-model="audioStore.volumeInput" min="1" max="100" disabled />
         <p> {{ (audioStore.volumeInput + '%') }} </p>
     </div>
-</Transition>
+</Transition> -->
 </template>
 
 <style scoped>
@@ -47,6 +60,54 @@ const audioStore = useAudioStore();
     border: var(--empty-border);
     border-radius: 10px;
     transition: var(--default-transition);
+}
+
+.new-volume-meter {
+    position: fixed;
+    top: 5px;
+    left: calc(50% - 152px);
+    z-index: 9000;
+    width: 300px;
+    height: 40px;
+    background-color: var(--blue-one);
+    border: 2px solid var(--blue-cobalt);
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px 2px var(--blue-zero);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    color: black
+}
+.new-volume-meter input {
+    width: calc(100% - 110px);
+}
+
+.new-volume-meter button {
+    font-size: 15px;
+    width: 35px;
+    margin-left: 10px;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    bottom: 0.5px;
+    color: inherit;
+}
+.new-volume-meter span {
+    position: relative;
+    bottom: 0.5px;
+    cursor: default;
+    user-select: none;
+    font-family: 'Lexend', sans-serif;
+    font-size: 15px;
+    width: 55px;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: inherit;
 }
 
 .custom-cursor-sensitivity {
@@ -69,15 +130,6 @@ const audioStore = useAudioStore();
         45deg,
         var(--blue-cobalt) 0,
         var(--blue-cobalt) 30px,
-        black 30px,
-        black 60px
-    );
-}
-.custom-cursor-sensitivity.volume {
-    background: repeating-linear-gradient(
-        45deg,
-        grey 0,
-        grey 30px,
         black 30px,
         black 60px
     );
@@ -147,6 +199,19 @@ const audioStore = useAudioStore();
 }
 .cursorTitle-transition-enter-to, .cursorTitle-transition-leave-from {
     opacity: 1;
+}
+
+.gamepad-volumeMeter-transition-leave-active {
+    transition: opacity 0.75s, top 0.75s;
+    transition-delay: 0.75s;
+}
+.gamepad-volumeMeter-transition-enter-from, .gamepad-volumeMeter-transition-leave-to {
+    opacity: 0;
+    top: -70px;
+}
+.gamepad-volumeMeter-transition-enter-to, .gamepad-volumeMeter-transition-leave-from {
+    opacity: 1;
+    top: 5px;
 }
 
 @media (max-width: 500px) {
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import { defineConfig } from "vite";
 
 import { VitePWA } from 'vite-plugin-pwa';
 import { qrcode } from 'vite-plugin-qrcode';
-
-import * as fs from "fs";
-import * as path from 'path';
+import { resolveComponents, resolveFontAwesomeIcons } from './resolvers.js';
 
 import vue from "@vitejs/plugin-vue";
 import generateSitemap from 'vite-ssg-sitemap';
@@ -48,41 +46,6 @@ const VUEUSE_AUTO_IMPORTS = {
     ]
 }
 
-/**
- * This function resolves all components in the components folder with special conditions.
- * @param {String} name The name of the component.
- * @param {Boolean} ssr If true, the app is rendering on the server.
- */
-function resolveComponents(name = "", ssr = true) {
-    if(name === "") { return null; }
-    const allFiles = fs.readdirSync(path.resolve("src/components"), { recursive: true });
-
-    // This for loop parses all the components' filenames to ensure that we are reading an actual component.
-    for(let i = 0; i < allFiles.length; i++) {
-        let file = allFiles[i];
-        if(file.endsWith(".md") || file.endsWith(".vue")) {
-            allFiles[i] = file.replaceAll("\\", "/");
-        } else {
-            allFiles.splice(i, 1);
-            i--;
-        }
-    }
-
-    // This for loop actually finds and sends all the components to where they are called.
-    const fileIndex = allFiles.findIndex((item) => {
-        return (name === item.split("/").pop().split(".")[0]);
-    });
-
-    if(fileIndex == -1) { return null; }
-    const filename = allFiles[fileIndex];
-
-    if(filename.endsWith(".client.vue") && ssr) {
-        return null;
-    } else {
-        return { name: "default", from: ("@/components/" + filename) }
-    }
-}
-
 export default defineConfig(({ isSsrBuild }) => {
     return {
         base: "/",
@@ -108,12 +71,8 @@ export default defineConfig(({ isSsrBuild }) => {
                 dts: './dts/components.d.ts',
                 extensions: ['vue', 'client.vue', 'md'],
                 resolvers: [
-                    (name) => { return resolveComponents(name, isSsrBuild) },
-                    (name) => {
-                        if(name === "FontAwesomeIcon") {
-                            return { name: "FontAwesomeIcon", from: '@fortawesome/vue-fontawesome' }
-                        }
-                    }
+                    (name) => { return resolveComponents(name, isSsrBuild); },
+                    (name) => { return resolveFontAwesomeIcons(name); }
                 ]
             }),
             Markdown({
