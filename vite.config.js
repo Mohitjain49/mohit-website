851c48962db044e1a9e0451b045ea2ac1a2d6683
Mohitjain49 <mohitkjain49@gmail.com>
2026-03-24 13:44:44 -0400

    Configuration Updates.
    - I ran upgrade.mjs to update the project dependencies and version number. I excluded both vite and beasties from the updates.
    - I updated the GitHub Workflow files so that they use "npm ci" (clean-install) instead of "npm install".
    - The Copyright page footer button now applies a proper offset so that the navigation bar doesn't overlap some elements. There is also a function that handles scrolling to the footer itself.

--- vite.config.js
+++ vite.config.js
@@ -94,7 +94,7 @@ export default defineConfig(({ isSsrBuild }) => {
                 includeAssets: ['**/*.woff2', '**/*.woff'],
 
                 workbox: {
-                    cacheId: `v3.8.1-${Date.now()}`,
+                    cacheId: `v3.8.2-${Date.now()}`,
                     globPatterns: ['**/*.{js,css,html,mjs,png,svg,pdf,webp,jpg,jpeg,woff2,woff,ttf,eot,md,wav,xml,txt,xsl,mp3}'],
                     maximumFileSizeToCacheInBytes: 5000000,
                     navigateFallback: "/index.html",
