6ec0450bc2b6490b31bd92f128f1834a48bbba53
Mohitjain49 <mohitkjain49@gmail.com>
2026-07-29 12:50:51 -0400

    Gamepad Bugfix.
    - When the user opens a website menu with a gamepad button click while in a QR Code Popup, it should now close the QR Code Popup and open the Website Menu in its place.
    - Updated Dependencies one more time before online push.

--- app/stores/WebsiteData.js
+++ app/stores/WebsiteData.js
@@ -320,13 +320,11 @@ export const useWebsiteDataStore = defineStore("web-data", () => {
         const route = router.currentRoute.value;
 
         if(qrdata === "quit" || qrdata === "") {
-            router.push({ path: route.path, hash: route.hash, query: { ...route.query, qrdata: undefined } }).then(() => {
-                sleep(10).then(() => { closeNavMenu(); });
-            });
+            router.push({ path: route.path, hash: route.hash, query: { ...route.query, qrdata: undefined }});
         } else if(qrdata === "toggle") {
             setQRCodePopup(showSharePopup.value ? "quit" : "main");
         } else {
-            router.push({ path: route.path, hash: route.hash, query: { ...route.query, qrdata } }).then(() => {
+            router.push({ path: route.path, hash: route.hash, query: { ...route.query, qrdata }}).then(() => {
                 sleep(10).then(() => { closeNavMenu(); });
             });
         }
